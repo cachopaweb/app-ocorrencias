9586d8f04d7beaccbb4542c4be79fd995231835c
Iago Sauer <iago_ortiz@hotmail.com>
2023-03-22 10:12:24 -0400

    BurnDown Funcionando

--- src/componentes/Graficos/index.js
+++ src/componentes/Graficos/index.js
@@ -3,36 +3,20 @@ import { Chart } from "react-google-charts";
 import { Container } from "./styles";
 
 function Graficos({ titulo, tipo = 'AreaChart', data }) {
-    const [options, setOptions] = useState({});
-    const [carregando, setCarregando] = useState(false);
-    function renderChart() {
-        setOptions({
-            title: titulo,
-            hAxis: { title: "Dias", titleTextStyle: { color: "#333" } },
-            vAxis: { minValue: 0 },
-            chartArea: { width: "50%", height: "70%" },
-        });
-    }
-
-    useEffect(() => {
-        setCarregando(true);
-        renderChart();
-        setCarregando(false);
-    }, [data])
-
     return (
-        <Container>
-            {carregando ?
-                <h1>Aguarde, carregando gráfico...</h1> :
-                <Chart
-                    chartType={tipo}
-                    width="100%"
-                    height="400px"
-                    data={data}
-                    options={options}
-                />
-            }
-        </Container>
+        <Chart
+            chartType={tipo}
+            width="100%"
+            height="400px"
+            data={data}
+            options={{
+                title: titulo,
+                hAxis: { title: "Dias", titleTextStyle: { color: "#333" } },
+                vAxis: { minValue: 0 },
+                chartArea: { width: "50%", height: "70%" },
+            }}
+        />
+
     );
 }
 
