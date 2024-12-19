0b1e81242122fa7f7377a9085d177c36b68c4b98
cachopaweb <cachopaweb@gmail.com>
2024-12-19 18:16:14 -0400

    update endereço api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const api = axios.create({ baseURL: 'https://servidor-os-fboxwqyjfq-rj.a.run.app' });//api Delphi com Horse
+const api = axios.create({ baseURL: 'https://servidor-os-fboxwqyjfq-rj.a.run.app' });//api Delphi com Horse
 
-const api = axios.create({ baseURL: 'http://localhost:9001' });//api Delphi com Horse
+// const api = axios.create({ baseURL: 'http://localhost:9001' });//api Delphi com Horse
 
 export default api;
